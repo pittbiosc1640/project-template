7d981c80e6a1238c70cd18b0e8c8ae224347650e
Alex Maldonado <alex@scient.ing>
2026-03-06 16:11:47 -0500

    refactor: switch tests to use paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,36 @@
-import os
+from pathlib import Path
 
 import pytest
 
 from lab import enable_logging
 
-TEST_DIR = os.path.dirname(__file__)
-TMP_DIR = os.path.join(TEST_DIR, "tmp")
-FILE_DIR = os.path.join(TEST_DIR, "files")
+TEST_DIR: Path = Path(__file__).resolve().parent
+TMP_DIR: Path = TEST_DIR / "tmp"
+FILE_DIR: Path = TEST_DIR / "files"
 
 
 def pytest_sessionstart(session):
     r"""Called after the Session object has been created and
     before performing collection and entering the run test loop.
     """
-    os.makedirs(TMP_DIR, exist_ok=True)
+    TMP_DIR.mkdir(exist_ok=True)
 
 
 @pytest.fixture
-def test_dir():
-    return os.path.abspath(TEST_DIR)
+def test_dir() -> Path:
+    return TEST_DIR
 
 
 @pytest.fixture
-def tmp_dir():
-    return os.path.abspath(TMP_DIR)
+def tmp_dir() -> Path:
+    return TMP_DIR
 
 
 @pytest.fixture
-def file_dir():
-    return os.path.abspath(FILE_DIR)
+def file_dir() -> Path:
+    return FILE_DIR
 
 
 @pytest.fixture(scope="session", autouse=True)
-def turn_on_logging():
+def turn_on_logging() -> None:
     enable_logging(10)
